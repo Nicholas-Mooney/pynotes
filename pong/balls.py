dfad48878c869ecee8790ad1220e14f9623ab5ba
Nicholas-Mooney <60123281+Nicholas-Mooney@users.noreply.github.com>
2022-02-22 14:31:24 -0800

    Add files via upload

--- pong/balls.py
+++ pong/balls.py
@@ -1,23 +1,34 @@
 
 import pygame, os, sys
 from random import randint
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, DEFAULT_BALL_NUM
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, DEFAULT_BALL_NUM, RED, BLACK
 
 from display_text import display_text
 from ball_class import *
 
-RED = (255,0,0)
-BLACK = (0,0,0)
+#TODO LIST
+#add user input class - num_balls - mass?
+#inherits from displayText - method(get_input)()
 
+#new_velocity
+#old_velocity
+#calcs elastic
+#random location / velocity / mass
+
+#comments naming property getters/setters private vars
 def main():
+    #init
     pygame.init()
     main_surface = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
     main_clock = pygame.time.Clock()
+    pygame.display.set_caption('Show Text')
 
+    #addBalls
     ball_list = pygame.sprite.RenderPlain()
     for loop_control in range(0, DEFAULT_BALL_NUM):
         ball_list.add(Ball(main_surface,(randint(0+20, SCREEN_WIDTH-20), randint(0+20, SCREEN_HEIGHT-20)) ))
 
+    #welcome text
     display_text(main_surface, "welcome", 50,RED,300,200)
     pygame.time.wait(2000)
 
@@ -36,4 +47,8 @@ def main():
 if(__name__ == "__main__"):
     main()
 
+#update old velocity
+#flag for collision
+#compute new velocity
+#move
 
--- pong/display_text.py
+++ pong/display_text.py
@@ -1,9 +1,16 @@
 import pygame
+from constants import BLACK
 
+"""
+Take a surface, some text, the size and color of the text, and the
+  desired coordinates, and display the text on the surface
+"""
 def display_text( main_surface,text_to_display, font_size, color,text_x,text_y):
+    font = pygame.font.SysFont('Comic Sans MS', font_size)
+    text = font.render(text_to_display, True, color, BLACK)
 
-    pygame.font.init()
-    myfont = pygame.font.SysFont('Comic Sans MS', font_size)
-    main_surface = myfont.render(text_to_display, False, color)
+    textRect = text.get_rect()
+    textRect.center = (text_x, text_y)
 
-    main_surface.blit(main_surface,(text_x,text_y))
+    main_surface.blit(text, textRect)
+    pygame.display.flip()
